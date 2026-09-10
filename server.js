615e95ca4bec99a7c5c29d77977d5a92b43fe08a
Diaa <diaa@example.com>
2026-09-11 02:13:49 +0300

    serve updated index

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "مكتب_هبه_ابو_كيله_للمحاماة.html")
+    path.join(__dirname, "index.html")
   );
 });
 
